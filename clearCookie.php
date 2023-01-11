bd43206b6fa87050c0f01808730158e93098df3c
MarinaStarovoytova <rm_star@mail.ru>
2023-01-11 20:45:11 +0300

    clearCookie

---
+++ clearCookie.php
@@ -0,0 +1,14 @@
+<?php 
+$userName = $_COOKIE['user'];
+setcookie($userName.'_created', '', time() - 1);
+setcookie('user','',time() - 1);
+$_SESSION = array();
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+session_destroy();
+header ('Location: index.php');
